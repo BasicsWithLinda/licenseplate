738db8f4b41185703634310051c75cb1a60d7a47
basicswithlinda <lindanguyenn0@gmail.com>
2024-11-15 22:35:30 +1100

    now refines to only find the numberplate. could optimise by not brute forcing the dimensions

--- cca.py
+++ cca.py
@@ -6,8 +6,16 @@ import localisation
 
 # grouping connected regions
 label_image = measure.label(localisation.binary_car_image)  # measuring it against the bw picture
+
+# dimensions that the license plate can be
+# the plate height would be between 8% and 20%, and width would be 15% and 40%
+# in this example, ive brute forced the adjustments of the values as this image does not conform to usual restraints -- possibility to optimise this
+min_height, max_height, min_width, max_width = (0.08*label_image.shape[0], 0.6*label_image.shape[0], 0.15*label_image.shape[1], 0.7*label_image.shape[1])
+plate_object_coords = []
+plate_like_objects = []
+
 fig, (ax1) = plt.subplots(1)
-ax1.imshow(localisation.blue_car_image, cmap="gray")
+ax1.imshow(localisation.blue_car_image, cmap="gray");
 
 for region in regionprops(label_image):
     if region.area < 50:
@@ -15,8 +23,15 @@ for region in regionprops(label_image):
         continue
 
     # bounding where the numberplate could be with a red border
-    minRow, minCol, maxRow, maxCol = region.bbox
-    rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="red", linewidth=2, fill=False)
-    ax1.add_patch(rectBorder)
+    min_row, min_col, max_row, max_col = region.bbox
+    region_height = max_row - min_row
+    region_width = max_col - min_col
+
+    # refining to only license plates to have a border
+    if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
+        plate_like_objects.append(localisation.binary_car_image[min_row:max_row, min_col:max_col])
+        plate_object_coords.append((min_row, min_col, max_row, max_col))
+        rectBorder = patches.Rectangle((min_col, min_row), max_col-min_col, max_row-min_row, edgecolor="red", linewidth=2, fill=False)
+        ax1.add_patch(rectBorder)
 
 plt.show()
